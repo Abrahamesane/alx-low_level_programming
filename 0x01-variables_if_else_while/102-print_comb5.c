92e4ca6b97e45e60a8335204b44f9c0dae2fb7f7
Abrahamesane <abrahamesane@gmail.com>
2022-09-09 11:01:11 -0700

    Scripts for variables if, else and while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,14 +30,14 @@ while (h < 58)
 					putchar(' ');
 					putchar(e);
 					putchar(i);
-					if (h == 57 &&  g == 56 && e == 57 && i == 57)
+					if (h == 57 &&  g == 56 && e == 57 && i == 57i)
 						break;
 					putchar(',');
 					putchar(' ');
 				}
 				i++;
 			}
-			e++;		
+			e++;
 		}
 		g++;
 	}
