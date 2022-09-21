b45400c0e1e0148772813469988e0b8b92a67509
Abrahamesane <abrahamesane@gmail.com>
2022-09-21 03:20:17 -0700

    script

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * - you are not aloowed to use a
+	 * - you are not allowed to modify p
+	 * only one statement
