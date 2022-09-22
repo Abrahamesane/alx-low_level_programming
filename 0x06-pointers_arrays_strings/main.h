5624dfb52621e17aa68162452ddb7aa89e618394
Abrahamesane <abrahamesane@gmail.com>
2022-09-22 13:29:23 -0700

    scripts

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef _main_h_
 #define _main_h_
+#include <stdio.h>
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -14,5 +15,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char);
-int main(void);
+
 #endif
