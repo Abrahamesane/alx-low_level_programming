bb14482214ad42e4484f472787a7af872316ed51
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-04-15 10:53:27 +0100

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,49 +1,51 @@
 #include "search_algos.h"
 
 /**
-  * interpolation_search - Searches for a value in a sorted array of
-  *			integers using the Interpolation search algorithm.
-  * @array: Pointer to the first element of the array to search in.
-  * @size: Number of elements in array.
-  * @value: Value to search for.
-  *
-  * Return: -1, If the value is not present in array or if array is NULL.
-  *         Otherwise, the first index where the value is located.
-  */
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t x, a, b;
+	size_t pos, low, high;
+	double f;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
 
-	for (a = 0, b = size - 1; b >= a;)
+	low = 0;
+	high = size - 1;
+
+	while (size)
 	{
-		x = a + (((double)(b - a) / (array[b] - array[a])) * (value - array[a]));
-		if (x < size)
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
 		{
-			printf("Value checked array[%ld] = [%d]\n", x, array[x]);
-		}
-		else
-		{
-			printf("Value checked array[%ld] is out of range\n", x);
+			printf(" is out of range\n");
 			break;
 		}
-
-		if (array[x] == value)
-		{
-			return (x);
-		}
-		if (array[x] > value)
-		{
-			b = x - 1;
-		}
 		else
 		{
-			a = x + 1;
+			printf(" = [%d]\n", array[pos]);
 		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
 	}
 
 	return (-1);
